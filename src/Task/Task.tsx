5601ca818e3ba8ea8bc60cfe719d04bfe6ade984
Almazov Urmat <almazov.urmat@gmail.com>
2023-05-25 15:34:21 +0600

    added functionality task done in component Task

--- src/Task/Task.tsx
+++ src/Task/Task.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-interface IDoToListProps
-{
+interface IDoToListProps {
     id: string;
     title: string;
     removeTask?: React.MouseEventHandler;
+    doneTask?: React.MouseEventHandler;
 }
 
 const Task: React.FC<IDoToListProps> = (props) => {
+    const [isDone, setIsDone] = useState(false);
+
+    const doneTask = () => {
+        setIsDone(true);
+    };
 
     return (
         <div className="todo">
-            <span>{props.title}</span>
-            <span onClick={props.removeTask}>Delete</span>
+            <span className={`todoTitle ${isDone ? 'crosseLine' : ''}`}>{props.title}</span>
+            <div className="btnBlock">
+                <button className="btn btnDone" onClick={doneTask}>Done</button>
+                <button className="btn btnDelete" onClick={props.removeTask}>Delete</button>
+            </div>
         </div>
     );
 };
